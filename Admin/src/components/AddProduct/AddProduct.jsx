fb6283a3bdf4c2f7652d70fa328ed51cacabc06c
ambatinitheesh <168229030+ambatinitheesh@users.noreply.github.com>
2024-05-01 15:03:24 +0530

    Update AddProduct.jsx

--- Admin/src/components/AddProduct/AddProduct.jsx
+++ Admin/src/components/AddProduct/AddProduct.jsx
@@ -27,7 +27,7 @@ const AddProduct = () => {
         let product = productDetails;
         let formData = new FormData();
         formData.append('product',image);
-        await fetch('http://localhost:4000/upload',{
+        await fetch('https://mern-bookstore-xi.vercel.app/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',
@@ -38,7 +38,7 @@ const AddProduct = () => {
         {
             product.image = responseData.image_url;
             console.log(product);
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://mern-bookstore-xi.vercel.app/addproduct',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
